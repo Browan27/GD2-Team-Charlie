e52e819cad2ae3a5f23cfbaf9579d8fcd4b6dabe
kanestapler <kanestapler@gmail.com>
2016-09-14 10:40:59 -0500

    adding new enemies/fixing stuff.

--- Assets/Scripts/SpawningWaves.cs
+++ Assets/Scripts/SpawningWaves.cs
@@ -5,17 +5,21 @@ public class SpawningWaves : MonoBehaviour {
 
     public GameObject hazard;
     public GameObject badGuy;
+    public GameObject shootingBadGuy;
     public Vector3 spawnValues;
     public int numOfAstroids;
     public int numOfBadGuys;
+    public int numOfShootingBadGuys;
     public float waveWaitAstroids;
     public float waveWaitBadGuys;
+    public float waveWaitShootingBadGuys;
     public float startWait;
 
     void Start()
     {
         StartCoroutine(SpawnWaves());
         StartCoroutine(SpawnBadGuys());
+        StartCoroutine(SpawnShootingBadGuys());
     }
 
     IEnumerator SpawnWaves()
@@ -41,6 +45,18 @@ public class SpawningWaves : MonoBehaviour {
 
             Instantiate(badGuy, spawnPosition, spawnRotation);
             yield return new WaitForSeconds(waveWaitBadGuys);
+        }
+    }
+
+    IEnumerator SpawnShootingBadGuys() {
+        yield return new WaitForSeconds(startWait);
+        for (int i = 0; i < numOfShootingBadGuys; i++) {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            Quaternion spawnRotation = Quaternion.identity;
+            spawnRotation.y = -180.0f;
+
+            Instantiate(shootingBadGuy, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(waveWaitShootingBadGuys);
         }
     }
 }
