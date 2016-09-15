0f8c4ac60b07baf9039eec832838e3ab9f295a91
kanestapler <kanestapler@gmail.com>
2016-09-15 13:07:53 -0500

    Adding score UI

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ScoreKeeper : MonoBehaviour {
 
     private int score;
+    private Text scoreText;
 
-	// Use this for initialization
 	void Start () {
         score = 0;
-	}
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        UpdateScoreText();
+
+    }
 
     public void AddScore(int i) {
         score += i;
-        print (GetScore());
+        UpdateScoreText();
     }
 
     public int GetScore() {
         return score;
+    }
+
+    private void UpdateScoreText() {
+        scoreText.text = "Score: " + score;
     }
 }
