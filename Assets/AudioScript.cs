2ba88e98b32d796b09da93c31b9328fc30003d80
Ben <bhoskins@stu.jsu.edu>
2016-09-15 22:21:42 -0500

    fixed small problems

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioScript : MonoBehaviour {
+
+    // Use this for initialization
+    static AudioScript instance;
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
